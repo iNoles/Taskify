0c84f728c80c3edecbbbd5aedd78b6572668ff1c
Jonathan Steele <iNoles@users.noreply.github.com>
2023-06-10 23:51:17 -0400

    damn git!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-Up    id("com.android.application") version "8.1.0-beta05" apply false
+    id("com.android.application") version "8.1.0-beta05" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     id("app.cash.sqldelight") version "2.0.0-rc01" apply false
 }
