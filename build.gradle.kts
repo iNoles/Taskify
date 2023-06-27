9f0beef1a1848bf8ab30d7ad80e05f932ed9060b
Jonathan <iNoles@users.noreply.github.com>
2023-06-27 13:04:57 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "8.2.0-alpha09" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-    id("app.cash.sqldelight") version "2.0.0-rc01" apply false
+    id("app.cash.sqldelight") version "2.0.0-rc02" apply false
 }
