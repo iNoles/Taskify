181767e63b80d5be9acb0cfeafe05759da982a18
Jonathan Steele <iNoles@users.noreply.github.com>
2024-06-20 21:53:36 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.jonathansteele.tasklist"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.jonathansteele.tasklist"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
