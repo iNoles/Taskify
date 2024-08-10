34619aa2c35f31dae254df7bc70d057c4d16c2d7
Jonathan Steele <iNoles@users.noreply.github.com>
2024-08-10 12:22:19 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.jonathansteele.tasklist"
+    namespace = "com.jonathansteele.taskify"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.jonathansteele.tasklist"
+        applicationId = "com.jonathansteele.taskify"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
